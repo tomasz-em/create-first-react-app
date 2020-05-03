9b0b70d1a0ad53f94eaf35e7a6318d542e5b321a
Tomasz-eM <56788219+tomasz-em@users.noreply.github.com>
2020-05-03 14:54:55 +0200

    added new conditions for the correctness of data in the LoginForm
    * extended component state with new attributes:
       - distinguisher of email syntactic correctness
       - minimum password length
    * added functions to support data quality in both fields
       - fired when content changes in a specific field
       - checking the correctness of the email address
       - also password length
       - also a joint check of both fields of the form when submitted, so as not to duplicate the same logic in a few pre-defined conditions
    * extended logic of component LoginForm
      - verified correctness of the form data, when ONE or BOTH fields are submitted with text
      - uses the same function to verify in both places, to be still "DRY"
    * added some new content to display iside LoginForm
      - of course added new warnings about a wrong values, which are diplayed conditionally
    * added two new styles for LoginForm: bordered info and red star
    * some text changed inside ThisPageInfo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';    // możliwe użycie SASSa, po zmianie nazwy pliku i pobraniu "node-sass"...
 
 const ThisPageInfo = () => {  // KOMPONENT FUNKCYJNY, czyli BEZSTANOWY 
-
+    // wprost zwrot wartości z tej funkcji jako zawartość do wyrenderowania na stronie 
   return (
     <div>
       {/*
@@ -24,7 +24,8 @@ const ThisPageInfo = () => {  // KOMPONENT FUNKCYJNY, czyli BEZSTANOWY
         <li>przesłanie pustych pól nie powinno się udać</li>
         <li>wyświetlenie niezbędnych komunikatów o błędzie</li>
         <li>graficzna interpretacja trójstanowego stanu formularza: gotowy/błędny/zalogowany jako alegoria dla utartych znaczeń kolorów</li>
-        <li>(wprowadzić prostą weryfikację dla poprawnego adresu email)</li>
+        <li>prosta weryfikacja dla poprawnego adresu email</li>
+        <li>sprawdzana także minimalna długość hasła</li>
       </ul>
     </div>
   );
@@ -42,54 +43,85 @@ class LoginForm extends React.Component {  // KOMPONENT STANOWY
 
   state = {
     email: 'ktos@gdzies.pl',
-    password: 'pa$$',
+    password: 'pa$$w0rd',
+    passwordMinLength: 5,
     isLoginError: false,
     isSubmited: false,
     isEmptyEmail: false,
+    isValidEmail: true,
     isEmptyPassword: false  // można pisać nawet Z PRZECINKLIEM PO OSTATNIM ATRYBUCIE (zmienia się jedna linmia w gicie), 
-        // ES6 to przyjmuje (lub transpiler do ES5 zamieni jak trzeba?)
+        // ES6 to przyjmuje (lub transpiler do ES5 zamieni jak trzeba... o ile jest dostępny?)
   }
 
-  handleLogin = (evt) => {  // też dla submit używane
+  isPassedCorrectEmail = ( email = "" ) => {  // ewentualna wartość domyślna
+      // własna logika, użyta wcześniej w liście zadań z jQuery
+    if ( ( email.length < 1 ) || ( email.indexOf('@') < 1 ) || ( email.lastIndexOf('.') < 3 ) || ( email.lastIndexOf('.') > (email.length - 3) ) ) {
+      return false;
+    }
+  return true;  
+  }
+
+  isPasswordLengthCorrect = ( text = "" ) => {
+    if ( text.length >= this.state.passwordMinLength ) return true;
+  return false;  
+  }
+
+  warnAboutEmailAndPassword = () => {
+    if ( !this.isPassedCorrectEmail( this.state.email ) ) this.setState({ isValidEmail: false });
+    if ( !this.isPasswordLengthCorrect( this.state.password ) ) this.setState({ isTooShortPassword: true });
+  }
+
+  handleLogin = ( evt ) => {  // też dla submit używane
     evt.preventDefault();
     console.log('Clicked',  evt.target.name, evt.target.innerText );
     // console.log("STATE:", this.state);
-      if ( ( this.state.email.length > 0 ) && ( this.state.password.length > 0) ) {
-        this.setState({ 
-          isSubmited: true,
-          isEmptyEmail: false,
-          isEmptyPassword: false,
-          isLoginError: false
-       });
+      if ( ( this.state.email.length > 0 ) && ( this.state.password.length > 0) ) { // czy podano jakieś treści do OBU pól formularza?
+            // czy podano prawidłową strukturę adresu email oraz wymaganą "złożoność" dla hasła?
+        if ( this.isPassedCorrectEmail( this.state.email ) && ( this.isPasswordLengthCorrect( this.state.password ) ) ) {
+            this.setState({ 
+            isSubmited: true,
+            isEmptyEmail: false,
+            isValidEmail: true,
+            isEmptyPassword: false,
+            isTooShortPassword: false,
+            isLoginError: false
+         });
+        }
+        else {  //  weryfikacja JAKOŚCI podanych OBU treści z formularza -- zakładać NIEUDANĄ weryfikację poprawości pól
+          this.warnAboutEmailAndPassword();
+        }
       } 
-      else {
+      else {  // BRAK treści w formularzu, w JEDNYM lub KAŻDYM polu
         if ( this.state.email.length <= 0 ) this.setState({ isEmptyEmail: true, isLoginError: true });
         if ( this.state.password.length <= 0 ) this.setState({ isEmptyPassword: true, isLoginError: true });
+          // też weryfikacja dla JAKOŚCI treści, możliwe przesłanie tylko jednego elementu
+          // DRY!
+        this.warnAboutEmailAndPassword();
       }
   }
 
-  handleEmailChange = (evt) => {
+  handleEmailChange = ( evt ) => {
     // evt.preventDefault();
     // this.email = evt.target.value;  // które THIS?!
     this.setState({ email: evt.target.value });
-      if ( evt.target.value.length > 0 ) this.setState({ isEmptyEmail: false });
+      if ( evt.target.value.length > 0 ) this.setState({ isEmptyEmail: false });  // ukryj komunikat o pustej zawartości
+      if ( this.isPassedCorrectEmail( evt.target.value ) ) this.setState({ isValidEmail: true });  // ukryj komunikat o nieprawidłowej strukturze
     // console.log('Changed', evt.target.name, evt.target.value );
   }
 
-  handlePassChange = (evt) => {
+  handlePassChange = ( evt ) => {
     // evt.preventDefault();
     // this.password = evt.target.value;
     this.setState({ password: evt.target.value });
       if ( evt.target.value.length > 0 ) this.setState({ isEmptyPassword: false });
-    // console.log('Changed', evt.target.name, evt.target.value );
+      if ( this.isPasswordLengthCorrect( evt.target.value ) ) this.setState({ isTooShortPassword: false });
   }
 
-  handleLogout = (evt) => {
+  handleLogout = ( evt ) => {
     // evt.preventDefault();
     // this.password = evt.target.value;
     this.setState({ email: '', password: '', isSubmited: false });
- 
-    // console.log('Changed', evt.target.name, evt.target.value );
+      // nie potrzeba zmieniać powyżej żadnego innego atrybutu stanu, bo podczas zalogowania są już one resetowane :)  
   }
 
   render() {  // Z OBSŁUGĄ WARUNKOWEGO WYŚWIETLANIA ZAWARTOŚCI KOMPONENTU, TAKŻE WYBRANE OBSZARY KOMPONENTU.. zależnie od warunku 
@@ -99,18 +131,27 @@ class LoginForm extends React.Component {  // KOMPONENT STANOWY
 
         { !this.state.isSubmited && (  // warunkowe wyświetlanie fragmentu danego komponentu WEWNĄTRZ danego komponentu - wariant FALSE
           <>
-            <h2>Podaj swoje poświadczenia</h2> 
-            <label htmlFor="email">e-mail</label>
-                {/* uwaga na atrybut "for"! zamiast tego słowa kluczowego tu MUSI BYĆ UŻYTE "htmlFor"  */}
+            <h2>Podaj swoje poświadczenia</h2>
+            <p className="info">Oba pola są wymagane. Email musi mieć prawidłową strukturę, a hasło tworzyć 
+            minimum {this.state.passwordMinLength} znaków.</p>
+            <label htmlFor="email">email <span>*</span></label>
+                {/* UWAGA na atrybut "for"! zamiast tego słowa kluczowego tu MUSI BYĆ UŻYTE "htmlFor"  */}
             <input type="text" id="email" name="email" onChange={this.handleEmailChange} value={this.state.email} />
             { this.state.isEmptyEmail ? (
               <p className="error-text">Pole emaila jest wymagane!</p> 
             ) : (null) }
+            { !this.state.isValidEmail ? (
+              <p className="error-text">Podano nieprawidłowy adres email!</p> 
+            ) : (null) }
 
-            <label htmlFor="pass">hasło</label>
+
+            <label htmlFor="pass">hasło <span>*</span></label>
             <input type="password" id="pass" name="pass" onChange={this.handlePassChange} value={this.state.password} />
             { this.state.isEmptyPassword ? (
               <p className="error-text">Pole hasła jest wymagane!</p> 
+            ) : (null) }
+            { this.state.isTooShortPassword ? (
+              <p className="error-text">Hasło jest zbyt krótkie!</p> 
             ) : (null) }
 
             <input type="submit" value="Zaloguj" /* onClick={this.handleClick} */ />
@@ -119,16 +160,20 @@ class LoginForm extends React.Component {  // KOMPONENT STANOWY
 
         { this.state.isSubmited && (  //  warunkowe wyświetlanie fragmentu danego komponentu WEWNĄTRZ danego komponentu - wariant TRUE -- "zalogowany"
           <div>
-            <h2>Jesteś zalogowany</h2> 
-            <h3>Przesłana zawartość</h3> 
-            <p>
-              <label>e-mail:</label>
-              <code>{this.state.email}</code>
-              <br />
-              <label>hasło:</label>
-              <code>{this.state.password}</code>
-            </p>
-            <button onClick={this.handleLogout}>Wyloguj</button>
+            <h2>Jesteś zalogowany</h2>
+            <h3>Brawo! W końcu udało Ci się trafić w ten duży przycisk ;)</h3>
+            <br />
+            <section className="info">
+              <h3>Przesłana zawartość</h3> 
+              <p>
+                <label>email:</label>
+                <code>{this.state.email}</code>
+                <br />
+                <label>hasło:</label>
+                <code>{this.state.password}</code>
+              </p>
+            </section>
+              <button onClick={this.handleLogout}>Wyloguj</button>
           </div>  
         )}
         </form>
